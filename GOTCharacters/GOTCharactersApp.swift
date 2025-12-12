b181b5935d74a785082effc9e5276333e53d8b3e
Stu Welsh <stuart@testcard.com>
2025-12-12 09:29:52 +0000

    Initial Commit

---
+++ GOTCharacters/GOTCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GOTCharactersApp.swift
+//  GOTCharacters
+//
+//  Created by Stu Welsh on 12/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct GOTCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GOTCharactersTests/GOTCharactersTests.swift
@@ -0,0 +1,17 @@
+//
+//  GOTCharactersTests.swift
+//  GOTCharactersTests
+//
+//  Created by Stu Welsh on 12/12/2025.
+//
+
+import Testing
+@testable import GOTCharacters
+
+struct GOTCharactersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
